e112b2840febc94ad29c0aa327f4e0f6af1d7690
Guy Marriott <guy@chec.io>
2021-03-31 10:25:21 -0700

    Update to support new variant request payload when adding to cart

--- src/features/cart.js
+++ src/features/cart.js
@@ -77,14 +77,41 @@ class Cart {
   /**
    * @param {Object|number} productId
    * @param {number} quantity
-   * @param {Object} variant
+   * @param {Object|string|null} variantData Either a string variant ID, or an object map of (variant) group IDs to
+   *                                         option IDs
    * @returns {Promise}
    */
-  add(productId, quantity = 1, variant = null) {
+  add(productId, quantity = 1, variantData = null) {
+    const validatedVariant = {};
+
+    if (typeof variantData === 'string' && variantData.startsWith('vrnt')) {
+      validatedVariant.variant_id = variantData;
+    } else if (variantData && typeof variantData === 'object') {
+      // Check that keys/values are IDs
+      const validKeys = Object.keys(variantData).every(key =>
+        key.startsWith('vgrp'),
+      );
+      const validValues = Object.values(variantData).every(key =>
+        key.startsWith('optn'),
+      );
+
+      if (!validKeys || !validValues) {
+        throw new Error(
+          'The variant options provided to cart.add do not appear to be a valid map of group IDs to option IDs',
+        );
+      }
+
+      validatedVariant.options = variantData;
+    } else if (variantData) {
+      throw new Error(
+        'Variant data provided to cart.add must be a variant ID, or a map of group IDs to option IDs',
+      );
+    }
+
     const data = {
       id: typeof productId === 'object' ? productId.id : productId,
       quantity,
-      variant,
+      ...validatedVariant,
     };
 
     return this.request('', 'post', data);
